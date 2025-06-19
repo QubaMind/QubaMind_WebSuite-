b944c7a360624fc4654afeceff754434608ee710
QubaMind <alkamaqu@gmail.com>
2025-06-19 23:41:21 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Paraphraser from "./pages/Paraphraser";
+import "./theme/theme.css"; // OR "./index.css"
+
+function App() {
+  return (
+    <div className="min-h-screen p-8">
+      <Paraphraser />
+    </div>
+  );
+}
+
+export default App;
